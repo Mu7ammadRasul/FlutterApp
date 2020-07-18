31638c7825b846d17b2d9006eb0867f1ce176e1c
Mu7ammadRasul <Mu7ammadRasul@users.noreply.github.com>
2020-07-18 20:20:41 +0300

    i added something

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    Material(
+      color: Colors.blueAccent,
+      child: Center(
+      child: Text("hey this is my first application",
+    textDirection: TextDirection.ltr,
+        )
+      )
+    )
+  );
+}
